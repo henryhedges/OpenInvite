2ed4292f1d843198d2ff0b258622f83aa290a048
MKS-students <mks.students@hackreactor.com>
2016-12-06 21:45:12 -0600

    test - chenged info in knex file

--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,10 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: '',
-      database: 'db',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PWD,
+      database: process.env.DB_NAME,
       charset: 'utf8'
     }
   },
